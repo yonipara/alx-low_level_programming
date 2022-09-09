d3f13f4d700183923017df160e1d26ad7bdb0a79
yonipara <yonisolo12@gmail.com>
2022-09-09 11:39:23 -0700

    print decimal numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int a = 48;
+
+while (a <= 57)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
