bb043417ee7b7fde24eba0413ed3f5b97c21583c
yonipara <yonisolo12@gmail.com>
2022-09-09 09:24:49 -0700

    lowwer case alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 97;
+int z = 122;
+
+while (a <= z)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
+
