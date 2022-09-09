1aecf11453b071bd71bb396c967418e6ced14367
yonipara <yonisolo12@gmail.com>
2022-09-09 14:56:42 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a = 48;
+int A = 97;
+
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+
+for (A = 97; A <= 102; A++)
+{
+putchar(A);
+}
+putchar('\n');
+return (0);
+}
