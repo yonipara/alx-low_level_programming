576a99d4cc04c34d1b8f668e56b8c8f9898a041e
yonipara <yonisolo12@gmail.com>
2022-09-09 15:49:18 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a = 97;
+
+for (a = 97; a <= 122; a++)
+{
+if (a == 101 || a == 113)
+{
+
+}
+else
+{
+putchar(a);
+}
+}
+putchar('\n');
+return (0);
+}
