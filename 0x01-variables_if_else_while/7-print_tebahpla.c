8289a8ef88ff125d4e08271fe7f4dd164e3cb642
yonipara <yonisolo12@gmail.com>
2022-09-09 12:05:30 -0700

    reverse alphabetical order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int z = 122;
+
+while (z >= 97)
+{
+putchar(z);
+z--;
+}
+putchar('\n');
+return (0);
+}
