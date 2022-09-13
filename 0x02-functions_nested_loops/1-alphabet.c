18458ddbc8d4a49849992fcd79b258dfada9e50c
yonipara <yonisolo12@gmail.com>
2022-09-13 14:38:49 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
+{
+int a = 97;
+int z = 122;
+
+while (a <= z)
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+}
