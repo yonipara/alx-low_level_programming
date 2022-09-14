66daa7eabfa3571eb1858909d0795d8c93db6b62
yonipara <yonisolo12@gmail.com>
2022-09-14 02:42:25 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_abs -> gives absolute values of integers
+ * @int: integer argument
+ * Return: (j);
+ */
+int _abs(int j)
+{
+if (j < 0)
+{
+j *= -1;
+}
+else if (j > 0)
+{
+j *= 1;
+}
+else
+{
+
+}
+return (j);
+}
