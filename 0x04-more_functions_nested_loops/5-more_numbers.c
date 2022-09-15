2067a4f994dff3a38447481d4bddf3316005bda6
yonipara <yonisolo12@gmail.com>
2022-09-15 12:43:31 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers -> prints same numbers consecutively in a new line
+ *
+ */
+void more_numbers(void)
+{
+int i = 0;
+int j = 0;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 14; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
