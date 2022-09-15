9fe86a28b42bad66e3a1e5d2d1968bd92d458614
yonipara <yonisolo12@gmail.com>
2022-09-15 11:10:32 -0700

    check digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,10 +2,11 @@
 /**
  * _isdigit -> checks if a character is a decimal digit
  * @c: an integer argument
+ * Return: 1 if success and 0 if failure
  */
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
+if ((c >= 48) && (c <= 57))
 {
 return (1);
 }
