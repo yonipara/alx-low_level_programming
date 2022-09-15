8a3e1fdf6e059bfd067c7a0d36966ceaa6b2ee0e
yonipara <yonisolo12@gmail.com>
2022-09-14 17:02:45 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdui.h>
+#include "main.h"
+/**
+ * print_to_98 -> prints all integers upto 98
+ * @n: integer argument
+ *
+ */
+void print_to_98(int n)
+{
+int x = 98;
+
+if (n == x)
+{
+printf("%d", n);
+}
+else
+{
+while (n >= x)
+{
+printf("%d, ", n);
+n--
+}
+while (n <= x)
+{
+printf("%d, ", n);
+n++
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,3 +17,5 @@ int print_last_digit(int);
 int add(int, int);
 
 void jack_bauer(void);
+
+void print_to_98(int n);
