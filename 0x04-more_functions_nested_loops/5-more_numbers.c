1a493ab88e47aa3273fdb80a206f9c6b019d7df8
yonipara <yonisolo12@gmail.com>
2022-09-16 03:45:53 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,23 +5,23 @@
  */
 void more_numbers(void)
 {
-int i = 0;
-int j = 0;
+int i;
+int j;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 14; j++)
 {
-if (j > 9)
+if (j <= 9)
 {
-_putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
+_putchar(j + '0');
 }
 else
 {
-_putchar(j + '0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 }
 }
 _putchar('\n');
 }
 }
-}
+
