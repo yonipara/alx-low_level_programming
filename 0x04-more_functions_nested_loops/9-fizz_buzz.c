7904a2d73613c403ca8de635fb677dbeab8c267a
yonipara <yonisolo12@gmail.com>
2022-09-16 14:47:13 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 15) == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
+printf(" ");
 }
 else if ((i % 5) == 0)
 {
-printf("Buzz ");
+printf("Buzz");
+printf(" ");
 }
 else if ((i % 3) == 0)
 {
-printf("Fizz ");
+printf("Fizz");
+printf(" ");
 }
 else
 {
 printf("%d", i);
+printf(" ");
 }
 }
 printf("\n");
