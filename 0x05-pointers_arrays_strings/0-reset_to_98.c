65ebf98b2eaf06ff474cda152185f8d70881648a
yonipara <yonisolo12@gmail.com>
2022-09-20 11:53:15 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 resets an int to 98.
+ * @n:is a pointer variable
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project is concerned with pointers, arrays and strings.
