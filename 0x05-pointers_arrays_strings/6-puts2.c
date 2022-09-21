6104961d355ba187727c1fc234fec0cef4935f7f
yonipara <yonisolo12@gmail.com>
2022-09-20 19:20:51 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character
+ * @str: input
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+int i = 0;
+
+for (; str[i] != '\0'; i++)
+{
+if ((i % 2) == 0)
+{
+_putchar(str[i]);
+}
+else
+{
+continue;
+}
+}
+_putchar('\n');
+}
