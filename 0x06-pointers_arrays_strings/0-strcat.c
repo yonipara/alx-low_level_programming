4bf8bbbbe97e60c59afbd1b8bff78f3325d82a71
yonipara <yonisolo12@gmail.com>
2022-09-22 11:13:56 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,24 +3,22 @@
  * _strcat -> concatenate two strings into one string.
  * @dest: is a pointer variable argument.
  * @src: is a pointer variable argument.
- * Return: p which is a pointer.
+ * Return: dest which is a pointer.
  */
 char *_strcat(char *dest, char *src)
 {
-char destination[100];
-int i, j;
-char *p = destination;
+int len = 0, i;
 
-for (i = 0; *dest != '\0'; i++)
+while (dest[len])
 {
-destination[i] = *dest;
-dest = dest + i;
+len++;
 }
-for (j = 0; *src != '\0'; j++)
+
+for (i = 0; src[i] != 0; i++)
 {
-i++;
-destination[i] = *src;
-src = src + j;
+dest[len] = src[i];
+len += 1;
 }
-return (p);
+dest[len] = '\0';
+return (dest);
 }
