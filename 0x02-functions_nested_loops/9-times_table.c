f2fa790d927894c8aa2595994535ff3e7cba9b1f
yonipara <yonisolo12@gmail.com>
2022-12-15 03:06:50 +0300

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table -> print a nines time table
+ * Return: nothing just prints
+ */
+void times_table(void)
+{
+int i = 0;
+
+while (i <= 9)
+{
+int j = 0;
+int x;
+while (j <= 9)
+{
+if (j != 9)
+{
+x = j *i;
+_putchar(x + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+x = j *i;
+_putchar(x + '0');
+}
+j++;
+}
+_putchar('\n');
+i++;
+}
+}
