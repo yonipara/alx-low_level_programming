441b8696902d984f05965f0e07119ee3fe332cca
yonipara <yonisolo12@gmail.com>
2022-12-16 16:03:09 +0300

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number -> prints all the decimal digits of an integer number
+ *
+ */
+int main(void)
+{
+print_number(98);
+return (0);
+}
+void print_number(int n)
+{
+printf("size of n: %lu\n", sizeof(n));
+}
