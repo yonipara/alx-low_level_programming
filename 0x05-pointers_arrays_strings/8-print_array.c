915902d0127c447e080be850db4b79848243aaa7
yonipara <yonisolo12@gmail.com>
2022-12-20 15:53:56 +0300

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array -> prints an array
+ * @a: a pointer argument for an array
+ * @n: an int argument
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+if (i == n - 1)
+{
+printf("%d\n", *(a + i));
+}
+else
+{
+printf("%d", *(a + i));
+printf(",");
+printf(" ");
+}
+}
+}
