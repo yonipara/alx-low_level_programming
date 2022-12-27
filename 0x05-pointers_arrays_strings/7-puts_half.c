126bcd4a9d1777444bb6f1ec6034bc2be27e5836
yonipara <yonisolo12@gmail.com>
2022-12-27 19:18:59 +0300

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,22 +15,22 @@ for (i = 0; str[i] != '\0'; i++)
 }
 if (i % 2 == 0)
 {
-	i = i / 2;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
+i = i / 2;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
 }
 else
 {
-	i = (i - 1) / 2;
-	i++;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-                i++;
-	}
+i = (i - 1) / 2;
+i++;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
 }
 _putchar('\n');
 }
