1393c7f5989d42449f7d652b5f8ef46572cde4b6
yonipara <yonisolo12@gmail.com>
2023-01-04 23:02:30 +0300

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -> prints a string recursivley, followed by a new line.
+ * @s: a pointer to the string
+ */
+void _puts_recursion(char *s)
+{
+if (*s !='\0')
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}	
+else
+{
+_putchar('\n');
+}
+}
