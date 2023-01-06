4ffb3c3a0aba3101c1d717598c669740b428d11d
yonipara <yonisolo12@gmail.com>
2023-01-06 20:03:39 +0300

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
 /**
- * main - prints the addition of all the arguments.
- * @argc: counts the number of arguments.
- * @argv: a string array argument.
- * Return: nothing
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: always 0 (syccess)
+ */
+
+int check_num(char *str)
+{
+unsigned int count;
+
+count = 0;
+while (count < strlen(str))
+{
+if (!isdigit(str[count]))
+{
+return (0);
+}
+
+count++;
+}
+return (1);
+}
+
+/**
+ * main - print the name of the program.
+ * @argc: count arguments.
+ * @argv: arguments.
+ * Return: always 0 (success)
  */
 int main(int argc, char *argv[])
 {
+int count;
+int str_to_int;
+int sum = 0;
 
-int sum, i;
-if (argc < 2)
+count = 1;
+while (count < argc)
 {
-printf("%d\n", 0);
+if (check_num(argv[count]))
+{
+str_to_int = atoi(argv[count]);
+sum += str_to_int;
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+count++;
 }
 
-sum = atoi(argv[1]);
-
-for (i = 2; i < argc; i++)
-{
-sum = sum + atoi(argv[i]);
-}
 printf("%d\n", sum);
+
 return (0);
 }
