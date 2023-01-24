12aeb886cdccc4d579d441bf752a020c47a07adf
yonipara <65731614+yonipara@users.noreply.github.com>
2023-01-24 18:33:50 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,13 +16,7 @@ float age;
 char *owner;
 };
 
-typedef struct
-{
-char *name;
-float age;
-char *owner;
-}
-dog_t;
+typedef struct dog dog_t;
   
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
