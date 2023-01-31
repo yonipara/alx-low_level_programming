7ba883e5b730622785cbfcf714dd25dd856c2b44
yonipara <yonisolo12@gmail.com>
2023-01-31 21:36:19 +0300

    pointer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,26 +8,27 @@ char *cap_string(char *str)
 {
 int i = 0;
 
-while (str[++i])
+for (i = 0; *(str + i) != '\0'; i++)
 {
-while (!(str[i] >= 'a' && str[i] <= 'z'))
+if (str[i] == ' ' ||
+str[i] == '\t' ||
+str[i] == '\n' ||
+str[i] == ',' ||
+str[i] == ';' ||
+str[i] == '.' ||
+str[i] == '!' ||
+str[i] == '?' ||
+str[i] == '"' ||
+str[i] == '(' ||
+str[i] == ')' ||
+str[i] == '{' ||
+str[i] == '}')
 {
-i++;
+if (*(str + i + 1) >= 97 && *(str + i + 1) <= 122)
+{
+*(str + i + 1) = *(str + i + 1) - 32;
 }
-if (str[i - 1] == ' ' ||
-str[i - 1] == '\t' ||
-str[i - 1] == '\n' ||
-str[i - 1] == ',' ||
-str[i - 1] == ';' ||
-str[i - 1] == '.' ||
-str[i - 1] == '!' ||
-str[i - 1] == '?' ||
-str[i - 1] == '"' ||
-str[i - 1] == '(' ||
-str[i - 1] == ')' ||
-str[i - 1] == '{' ||
-str[i - 1] == '}')
-str[i] -= 32;
+}
 }
 return (str);
 }
