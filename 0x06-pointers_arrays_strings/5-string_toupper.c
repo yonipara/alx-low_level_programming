b78f8e6d39fcabee49230c4f1e4db76244a91570
yonipara <yonisolo12@gmail.com>
2023-01-31 20:45:45 +0300

    pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,11 +8,11 @@ char *string_toupper(char *str)
 {
 int i = 0;
 
-while (str[i++])
+for (i = 0; *(str + i) != '\0'; i++)
 {
-if (str[i] >= 'a' && str[i] <= 'z')
+if (*(str + i) >= 97 && *(str + i) <= 122)
 {
-str[i] -= 32;
+*(str + i) = *(str + i) - 32;
 }
 }
 return (str);
