fca04dbb75aed65d32f0ebdbd42812d020f553ec
yonipara <yonisolo12@gmail.com>
2023-01-31 13:51:31 +0300

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,33 +7,22 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, diff = 0;
+int i = 0;
 
-while (1)
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-if (s1[i] == '\0' && s2[i] == '\0')
-{
-break;
-}
-else if (s1[i] == '\0')
-{
-diff = s2[i];
-break;
-}
-else if (s2[i] == '\0')
-{
-diff = s1[i];
-break;
-}
-else if (s1[i] != s2[i])
-{
-diff = s1[i] - s2[i];
-break;
-}
-else
+if (s1[i] == s2[i])
 {
 i++;
+continue;
 }
+
+else
+{
+return (s1[i] - s2[i]);
+break;
 }
-return (diff);
+
+}
+return (0);
 }
