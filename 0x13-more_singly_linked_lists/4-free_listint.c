f1fdeac605e0b9a10347c02a9b39c39f80dc6cc5
yonipara <yonisolo12@gmail.com>
2023-03-28 13:24:20 +0300

    data structure

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,11 @@
  */
 void free_listint(listint_t *head)
 {
-listint_t *temp;
-
-while (head)
+listint_t *temp = head;
+while (head != NULL)
 {
-temp = head->next;
-free(head);
-head = temp;
+	temp = head->next;
+	free(head);
+	head = temp;
 }
 }
