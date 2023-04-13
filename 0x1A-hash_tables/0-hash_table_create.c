0cd6acc936996c89b1f1f48c0fbd8112a286c106
yonipara <yonisolo12@gmail.com>
2023-04-13 19:13:15 +0300

    hash table data structures

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,34 @@
 #include "hash_tables.h"
+
 /**
-  * hash_table_create - creates a hash table.
-  * @size: size of the hash table.
-  * Return: pointer to the hash table
-  */
+ * hash_table_create - creates a hash table
+ * @size: size of the array to be created
+ *
+ * Return: pointer to the newly created hash table, or NULL on failure
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i;
 	hash_table_t *new_table;
+	hash_node_t **new_array;
+	unsigned long int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
+
+	new_array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		new_array[i] = NULL;
+
 	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
 		return (NULL);
+
 	new_table->size = size;
-	new_table->array = malloc(size * sizeof(hash_node_t *));
-	if (!new_table->array)
-	{
-		free(new_table);
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
+	new_table->array = new_array;
+
 	return (new_table);
 }
