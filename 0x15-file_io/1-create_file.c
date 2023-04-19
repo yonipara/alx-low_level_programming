83b91c196c4618bec535b867f98ca00e8d92cdd8
yonipara <yonisolo12@gmail.com>
2023-04-19 20:42:44 +0300

    filei/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
@@ -10,25 +9,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd, i;
+	int len = 0;
 
-	if (filename == NULL)
-		return (-1);
+	fd = open(filename, O_WRONLY|O_TRUNC|O_CREAT, 0600);
 
-	if (text_content != NULL)
+	if (fd != -1)
 	{
-		for (len = 0; text_content[len];)
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
 			len++;
+		}
+
+		write(fd, text_content, len);
+
+		write(fd, '\0', 1);
+
+		close(fd);
+
+		return (1);
 	}
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-
-	if (fd == -1 || w == -1)
+	else
+	{
 		return (-1);
-
-	close(fd);
-
-	return (1);
+	}
 }
 
