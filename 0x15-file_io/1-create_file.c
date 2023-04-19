a8b38daf8f8dd07701e0a5c4b43fc78abcd3f53f
yonipara <yonisolo12@gmail.com>
2023-04-19 20:55:10 +0300

    filei/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,9 @@ int create_file(const char *filename, char *text_content)
 	int fd, i;
 	int len = 0;
 
+	if (filename == NULL)
+                return (-1);
+	
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 
 	if (fd != -1)
@@ -22,11 +25,6 @@ int create_file(const char *filename, char *text_content)
 		}
 
 		write(fd, text_content, len);
-
-		if (write == -1)
-		{
-			return (-1);
-		}
 
 		write(fd, '\0', 1);
 
