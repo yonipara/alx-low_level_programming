aa5ed0b6b9a5d1007237cb290a46cc25cdc83cd2
yonipara <yonisolo12@gmail.com>
2023-04-20 16:07:44 +0300

    filei/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,14 +14,6 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
-	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
-
-	if (fd2 == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
 	fd1 = open(argv[1], O_RDONLY);
 
 	if (fd1 == -1)
@@ -48,6 +40,15 @@ int main(int argc, char **argv)
 	}
 
 	buffer[i] = '\0';
+
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+
+        if (fd2 == -1)
+        {
+                dprintf(2, "Error: Can't write to %s\n", argv[2]);
+                free(buffer);
+		exit(99);
+        }
 
 	j = write(fd2, buffer, i);
 
